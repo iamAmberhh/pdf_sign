b67fa038c93b660081110add150a0abb56b59a13
Amber <melody19941231@gmail.com>
2022-11-13 16:37:31 +0800

    Update 2022-11-13T08:37:29.243Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -6,7 +6,7 @@ function _asyncToGenerator(fn) { return function () { var self = this, args = ar
 
 var footer = document.querySelector(".footer");
 
-if (window.location.pathname == "/pdf_sign/") {
+if (window.location.pathname == "/pdf_sign/" || window.location.pathname == '/pdf_sign/index.html') {
   footer.classList.add("d-none");
 } // 下bar 時間軸
 
@@ -14,14 +14,7 @@ if (window.location.pathname == "/pdf_sign/") {
 var timeline = document.querySelectorAll(".timeline li");
 var pages = 0;
 
-if (window.location.pathname == "/signup.html") {
-  //'/pdf_sign/upload.html'
-  pages = 1;
-  timelineCheck();
-}
-
-if (window.location.pathname == "/download.html") {
-  pages = 2;
+if (window.location.pathname == '/pdf_sign/upload.html') {
   timelineCheck();
 }
 
@@ -98,8 +91,6 @@ function drop(e) {
   handleFiles(files, fileObj);
 }
 
-var uploadSuccess = new bootstrap.Modal(document.querySelector("#uploadSuccess"));
-
 function handleFiles(files, fileObj) {
   if (files[0].name.split(".")[1] != "pdf") {
     var notFileAlert = new bootstrap.Modal(document.querySelector("#notFileAlert"));
@@ -133,14 +124,24 @@ function handleFiles(files, fileObj) {
     renderUpload();
     uploadSuccess.show();
   }
-}
+} // 點擊上傳
 
+
+var uploadSuccess = new bootstrap.Modal(document.querySelector("#uploadSuccess"));
 var fileUploader = document.querySelector(".select");
 
 if (fileUploader) {
   fileUploader.addEventListener("change", function (e) {
     // 副檔名檢查
     if (fileUploader.files[0].name.split(".")[1] != "pdf") {
+      var notFileAlert = new bootstrap.Modal(document.querySelector("#notFileAlert"));
+      notFileAlert.show();
+      return;
+    }
+
+    if (fileUploader.files[0].size > 10485760) {
+      var fileSizeOver = new bootstrap.Modal(document.querySelector("#fileSizeOver"));
+      fileSizeOver.show();
       return;
     }
 
@@ -171,13 +172,10 @@ var fileTableBody = document.querySelector(".table-body");
 
 function renderUpload() {
   var str = "";
-  var file = "";
   data.forEach(function (i) {
-    str += "<tr data-upload>\n    <th scope=\"row\"><a href=\"#\">".concat(i.name, "</a></th>\n    <td><a href=\"#\">").concat(i.year, "/").concat(i.month, "/").concat(i.date, "</a></td>\n    <td class=\"d-none d-md-block\"><a href=\"#\">--</a></td>\n</tr>");
-    file += "".concat(i.name);
+    str += "<tr data-upload>\n    <th scope=\"row\"><a href=\"#\" class=\"ellipsis\">".concat(i.name, "</a></th>\n    <td><a href=\"#\">").concat(i.year, "/").concat(i.month, "/").concat(i.date, "</a></td>\n    <td class=\"d-none d-md-block\"><a href=\"#\">--</a></td>\n</tr>");
   });
   fileTableBody.innerHTML = str;
-  fileName.textContent = file;
 }
 
 var nextStepBtn = document.querySelector("[data-next]");
@@ -192,8 +190,11 @@ if (fileTableBody) {
       i.classList.remove("bg-light");
     });
     e.target.closest("tr").classList.add("bg-light");
+    console.log(e.target.closest('tr').firstChild.nextSibling.firstChild.textContent);
+    var file = e.target.closest('tr').firstChild.nextSibling.firstChild.textContent;
     nextStepBtn.classList.remove("disabled", "btn-disabled");
     nextStepBtn.classList.add("btn-secondary");
+    fileName.textContent = file;
   });
 }
 
@@ -204,6 +205,8 @@ if (nextStepBtn) {
     nextStepBtn.textContent = "\u5275\u5EFA\u6587\u4EF6";
     nextStepBtn.classList.add("disabled", "btn-disabled");
     nextStepBtn.classList.remove("btn-secondary");
+    pages++;
+    timelineCheck();
   });
 }
 
@@ -214,161 +217,8 @@ if (noUpload) {
     noUpload.classList.remove("d-none");
     fileTable.classList.add("d-none");
   }
-} // PDF渲染成畫布
-// const selectFile = document.querySelector(".select");
-// pdfjsLib.GlobalWorkerOptions.workerSrc = "https://mozilla.github.io/pdf.js/build/pdf.worker.js";
-// const canvas = document.querySelector("#canvas");
-// const ctx = canvas.getContext("2d");
-// function renderPDF(data) {
-//   //  非同步 取得PDF檔案 進到then
-//   pdfjsLib.getDocument(data).promise.then((doc) => {
-//     //  選擇檔案的第一頁，成功再進到then
-//     doc.getPage(1).then((page) => {
-//       //  取得檔案的大小，並設為視口大小
-//       const viewport = page.getViewport({ scale: 1 });
-//       //   讓畫布和視口(檔案)大小一樣
-//       canvas.width = viewport.width;
-//       canvas.height = viewport.height;
-//       //   渲染頁面
-//       page.render({
-//         canvasContext: ctx,
-//         viewport: viewport
-//       });
-//     });
-//   });
-// }
-// if(selectFile){
-//   selectFile.addEventListener("change", (e) => {
-//     if (e.target.files[0] === undefined) return;
-//     const file = e.target.files[0];
-//     const fileReader = new FileReader();
-//     fileReader.readAsArrayBuffer(file);
-//     fileReader.addEventListener("load", () => {
-//       // 獲取readAsArrayBuffer產生的結果，並用來渲染PDF
-//       const typedarray = new Uint8Array(fileReader.result);
-//       renderPDF(typedarray);
-//     });
-//   });
-// }
+} // 簽名
 
-
-var Base64Prefix = "data:application/pdf;base64,";
-var add = document.querySelector(".add");
-pdfjsLib.GlobalWorkerOptions.workerSrc = "https://mozilla.github.io/pdf.js/build/pdf.worker.js"; // 使用原生 FileReader 轉檔
-
-function readBlob(blob) {
-  return new Promise(function (resolve, reject) {
-    var reader = new FileReader();
-    reader.addEventListener("load", function () {
-      return resolve(reader.result);
-    });
-    reader.addEventListener("error", reject);
-    reader.readAsDataURL(blob);
-  });
-}
-
-function printPDF(_x) {
-  return _printPDF.apply(this, arguments);
-}
-
-function _printPDF() {
-  _printPDF = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee(pdfData) {
-    var data, pdfDoc, pdfPage, viewport, canvas, context, renderContext, renderTask;
-    return regeneratorRuntime.wrap(function _callee$(_context) {
-      while (1) {
-        switch (_context.prev = _context.next) {
-          case 0:
-            _context.next = 2;
-            return readBlob(pdfData);
-
-          case 2:
-            pdfData = _context.sent;
-            // 將 base64 中的前綴刪去，並進行解碼
-            data = atob(pdfData.substring(Base64Prefix.length)); // 利用解碼的檔案，載入 PDF 檔及第一頁
-
-            _context.next = 6;
-            return pdfjsLib.getDocument({
-              data: data
-            }).promise;
-
-          case 6:
-            pdfDoc = _context.sent;
-            _context.next = 9;
-            return pdfDoc.getPage(1);
-
-          case 9:
-            pdfPage = _context.sent;
-            // 設定尺寸及產生 canvas
-            viewport = pdfPage.getViewport({
-              scale: window.devicePixelRatio
-            });
-            canvas = document.createElement("canvas");
-            context = canvas.getContext("2d"); // 設定 PDF 所要顯示的寬高及渲染
-
-            canvas.height = viewport.height;
-            canvas.width = viewport.width;
-            renderContext = {
-              canvasContext: context,
-              viewport: viewport
-            };
-            renderTask = pdfPage.render(renderContext); // 回傳做好的 PDF canvas
-
-            return _context.abrupt("return", renderTask.promise.then(function () {
-              return canvas;
-            }));
-
-          case 18:
-          case "end":
-            return _context.stop();
-        }
-      }
-    }, _callee);
-  }));
-  return _printPDF.apply(this, arguments);
-}
-
-function pdfToImage(_x2) {
-  return _pdfToImage.apply(this, arguments);
-} // 此處 canvas 套用 fabric.js
-// const canvas = new fabric.Canvas("canvas");
-// document.querySelector(".select").addEventListener("change", async function(e){
-//   canvas.requestRenderAll();
-//   const pdfData = await printPDF(e.target.files[0]).promise;
-//   const pdfImage = await pdfToImage(pdfData).promise;
-//   // 透過比例設定 canvas 尺寸
-//   canvas.setWidth(pdfImage.width / window.devicePixelRatio);
-//   canvas.setHeight(pdfImage.height / window.devicePixelRatio);
-//   // 將 PDF 畫面設定為背景
-//   canvas.setBackgroundImage(pdfImage, canvas.renderAll.bind(canvas));
-// });
-// 簽名
-
-
-function _pdfToImage() {
-  _pdfToImage = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee2(pdfData) {
-    var scale;
-    return regeneratorRuntime.wrap(function _callee2$(_context2) {
-      while (1) {
-        switch (_context2.prev = _context2.next) {
-          case 0:
-            // 設定 PDF 轉為圖片時的比例
-            scale = 1 / window.devicePixelRatio; // 回傳圖片
-
-            return _context2.abrupt("return", new fabric.Image(pdfData, {
-              id: "renderPDF",
-              scaleX: scale,
-              scaleY: scale
-            }));
-
-          case 2:
-          case "end":
-            return _context2.stop();
-        }
-      }
-    }, _callee2);
-  }));
-  return _pdfToImage.apply(this, arguments);
-}
 
 var signCanvas = document.querySelector("#sign-canvas");
 
@@ -478,5 +328,143 @@ if (signCanvas) {
   clearBtn.addEventListener("click", reset);
   backBtn.addEventListener("click", back);
   saveBtn.addEventListener("click", save);
+} // PDF渲染成畫布
+
+
+var Base64Prefix = "data:application/pdf;base64,";
+pdfjsLib.GlobalWorkerOptions.workerSrc = "https://mozilla.github.io/pdf.js/build/pdf.worker.js"; // 使用原生 FileReader 轉檔
+
+function readBlob(blob) {
+  return new Promise(function (resolve, reject) {
+    var reader = new FileReader();
+    reader.addEventListener("load", function () {
+      return resolve(reader.result);
+    });
+    reader.addEventListener("error", reject);
+    reader.readAsDataURL(blob);
+  });
 }
+
+function printPDF(_x) {
+  return _printPDF.apply(this, arguments);
+}
+
+function _printPDF() {
+  _printPDF = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee2(pdfData) {
+    var data, pdfDoc, pdfPage, viewport, canvas, context, renderContext, renderTask;
+    return regeneratorRuntime.wrap(function _callee2$(_context2) {
+      while (1) {
+        switch (_context2.prev = _context2.next) {
+          case 0:
+            _context2.next = 2;
+            return readBlob(pdfData);
+
+          case 2:
+            pdfData = _context2.sent;
+            // 將 base64 中的前綴刪去，並進行解碼
+            data = atob(pdfData.substring(Base64Prefix.length)); // 利用解碼的檔案，載入 PDF 檔及第一頁
+
+            _context2.next = 6;
+            return pdfjsLib.getDocument({
+              data: data
+            }).promise;
+
+          case 6:
+            pdfDoc = _context2.sent;
+            _context2.next = 9;
+            return pdfDoc.getPage(1);
+
+          case 9:
+            pdfPage = _context2.sent;
+            // 設定尺寸及產生 canvas
+            viewport = pdfPage.getViewport({
+              scale: window.devicePixelRatio
+            });
+            canvas = document.createElement("canvas");
+            context = canvas.getContext("2d"); // 設定 PDF 所要顯示的寬高及渲染
+
+            canvas.height = viewport.height;
+            canvas.width = viewport.width;
+            renderContext = {
+              canvasContext: context,
+              viewport: viewport
+            };
+            renderTask = pdfPage.render(renderContext);
+            return _context2.abrupt("return", renderTask.promise.then(function () {
+              return canvas;
+            }));
+
+          case 18:
+          case "end":
+            return _context2.stop();
+        }
+      }
+    }, _callee2);
+  }));
+  return _printPDF.apply(this, arguments);
+}
+
+function pdfToImage(_x2) {
+  return _pdfToImage.apply(this, arguments);
+} // 此處 canvas 套用 fabric.js
+
+
+function _pdfToImage() {
+  _pdfToImage = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee3(pdfData) {
+    var scale;
+    return regeneratorRuntime.wrap(function _callee3$(_context3) {
+      while (1) {
+        switch (_context3.prev = _context3.next) {
+          case 0:
+            scale = 1 / window.devicePixelRatio;
+            return _context3.abrupt("return", new fabric.Image(pdfData, {
+              scaleX: scale,
+              scaleY: scale
+            }));
+
+          case 2:
+          case "end":
+            return _context3.stop();
+        }
+      }
+    }, _callee3);
+  }));
+  return _pdfToImage.apply(this, arguments);
+}
+
+var canvas = new fabric.Canvas("canvas");
+document.querySelector(".testBtn").addEventListener("change", /*#__PURE__*/function () {
+  var _ref = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee(e) {
+    var pdfData, pdfImage;
+    return regeneratorRuntime.wrap(function _callee$(_context) {
+      while (1) {
+        switch (_context.prev = _context.next) {
+          case 0:
+            canvas.requestRenderAll();
+            _context.next = 3;
+            return printPDF(e.target.files[0]);
+
+          case 3:
+            pdfData = _context.sent;
+            _context.next = 6;
+            return pdfToImage(pdfData);
+
+          case 6:
+            pdfImage = _context.sent;
+            canvas.setWidth(pdfImage.width / window.devicePixelRatio);
+            canvas.setHeight(pdfImage.height / window.devicePixelRatio);
+            canvas.setBackgroundImage(pdfImage, canvas.renderAll.bind(canvas));
+
+          case 10:
+          case "end":
+            return _context.stop();
+        }
+      }
+    }, _callee);
+  }));
+
+  return function (_x3) {
+    return _ref.apply(this, arguments);
+  };
+}());
 //# sourceMappingURL=all.js.map
